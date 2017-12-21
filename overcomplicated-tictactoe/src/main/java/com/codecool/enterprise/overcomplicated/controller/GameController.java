eb8df5b1a56fa471e3c4c081db2da3f7af8a6c10
Gergely Németh <ngerle@gmail.com>
2017-12-21 02:49:35 +0100

    Implement basic tic tac toe gameplay

--- overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/controller/GameController.java
+++ overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/controller/GameController.java
@@ -2,9 +2,14 @@ package com.codecool.enterprise.overcomplicated.controller;
 
 import com.codecool.enterprise.overcomplicated.model.Player;
 import com.codecool.enterprise.overcomplicated.model.TictactoeGame;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @SessionAttributes({"player", "game"})
@@ -30,21 +35,43 @@ public class GameController {
         return "welcome";
     }
 
-    @PostMapping(value="/changeplayerusername")
+    @PostMapping(value = "/changeplayerusername")
     public String changPlayerUserName(@ModelAttribute Player player) {
         return "redirect:/game";
     }
 
     @GetMapping(value = "/game")
-    public String gameView(@ModelAttribute("player") Player player, Model model) {
+    public String gameView(@ModelAttribute("player") Player player, Model model,
+                           @ModelAttribute("game") TictactoeGame game) {
+        game.initGame();
+        model.addAttribute("avatar_uri", getAvatarUri());
         model.addAttribute("funfact", "&quot;Chuck Norris knows the last digit of pi.&quot;");
         model.addAttribute("comic_uri", "https://imgs.xkcd.com/comics/bad_code.png");
         return "game";
     }
 
-    @GetMapping(value = "/game-move")
-    public String gameMove(@ModelAttribute("player") Player player, @ModelAttribute("move") int move) {
-        System.out.println("Player moved " + move);
+    @GetMapping(value = "/game/new")
+    public String gameView(@ModelAttribute("game") TictactoeGame game) {
+        game.startGame();
         return "redirect:/game";
+    }
+
+
+    @GetMapping(value = "/game-move")
+    public String gameMove(@ModelAttribute("player") Player player,
+                           @ModelAttribute("game") TictactoeGame game,
+                           @ModelAttribute("move") int move) {
+        game.playerMove(move);
+        if (game.isGameOver()) {
+            return "redirect:gameover";
+        }
+        return "redirect:game";
+    }
+
+    @GetMapping(value = "/gameover")
+    public String gameOver(@ModelAttribute("player") Player player,
+                           @ModelAttribute("game") TictactoeGame game) {
+        if (game.getWinner() == null) return "redirect:/";
+        return "gameover";
     }
 }
--- overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/model/TictactoeGame.java
+++ overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/model/TictactoeGame.java
@@ -1,4 +1,137 @@
 package com.codecool.enterprise.overcomplicated.model;
 
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@Component
 public class TictactoeGame {
+
+    private String[] table;
+    private String winner = null;
+
+    public boolean isGameOver() {
+        return gameOver;
+    }
+
+    private boolean gameOver = false;
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public String[] getTable() {
+        return table;
+    }
+
+    private long numOfFreeCells() {
+        return Arrays.stream(table)
+                .filter(c -> c.equals("-"))
+                .count();
+    }
+
+    private void populateTable() {
+        for (int i = 0; i < 9; i++) {
+            table[i] = "-";
+        }
+    }
+
+    public void initGame() {
+        if (table == null) {
+            startGame();
+        }
+    }
+
+    public void startGame() {
+        winner = null;
+        gameOver = false;
+        table = new String[9];
+        populateTable();
+    }
+
+    public void playerMove(int move) {
+        if (isMoveValid(move) || winner != null) {
+            table[move] = "O";
+            System.out.println("Player moved " + move);
+            checkWinner();
+            if (winner == null) {
+                aiMove();
+            }
+        }
+    }
+
+    private void checkWinner() {
+        List<List<Integer>> checklists = new ArrayList<>();
+
+        List<Integer> checkList1 = Arrays.asList(0, 1, 2);
+        List<Integer> checkList2 = Arrays.asList(3, 4, 5);
+        List<Integer> checkList3 = Arrays.asList(6, 7, 8);
+
+        List<Integer> checkList4 = Arrays.asList(0, 3, 6);
+        List<Integer> checkList5 = Arrays.asList(1, 4, 7);
+        List<Integer> checkList6 = Arrays.asList(2, 5, 8);
+
+        List<Integer> checkList7 = Arrays.asList(0, 4, 8);
+        List<Integer> checkList8 = Arrays.asList(2, 4, 6);
+
+        checklists.add(checkList1);
+        checklists.add(checkList2);
+        checklists.add(checkList3);
+        checklists.add(checkList4);
+        checklists.add(checkList5);
+        checklists.add(checkList6);
+        checklists.add(checkList7);
+        checklists.add(checkList8);
+
+        for (List<Integer> list : checklists) {
+            int counterO = 0;
+            int counterX = 0;
+            for (Integer index : list) {
+                if (table[index].equals("O") ) {
+                    counterO++;
+                } else if (table[index].equals("X")) {
+                    counterX++;
+                }
+            }
+            if (counterO == 3) {
+                System.out.println("Player won");
+                gameOver = true;
+                winner = "You";
+                return;
+            } else if (counterX == 3) {
+                System.out.println("Computer won");
+                gameOver = true;
+                winner = "Computer";
+                return;
+            }
+        }
+        if (numOfFreeCells() == 0) {
+            gameOver = true;
+            winner = "Draw";
+        }
+    }
+
+    private boolean isMoveValid(int move) {
+        if (!table[move].equals("-")) {
+            System.out.println("Illegal move!");
+            return false;
+        }
+        return true;
+    }
+
+    private void aiMove() {
+        Random random = new Random();
+        int move = random.nextInt(9);
+        if (numOfFreeCells() > 0) {
+            while (!isMoveValid(move) && numOfFreeCells() > 0) {
+                move = random.nextInt(9);
+            }
+            table[move] = "X";
+            System.out.println("Computer moved " + move);
+            checkWinner();
+        }
+    }
 }
